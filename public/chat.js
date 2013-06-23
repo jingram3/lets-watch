efacd8e6b36c18ad1adf7d49f0f03aa19d4632f7
Joseph Ingram <jstalvern@gmail.com>
2013-06-23 14:08:54 -0400

    titles

--- public/chat.js
+++ public/chat.js
@@ -1,9 +1,11 @@
 function onYouTubePlayerReady(playerId) {
     ytplayer = document.getElementById("myytplayer");
+    var id = getYoutubeID($('#myytplayer').attr('data'));
+    getTitle(id);
     ytplayer.playVideo();
     ytplayer.pauseVideo();
-    //var surl = 'http://localhost:5000/'; 
-    var surl = 'http://mighty-brook-9138.herokuapp.com/';
+    var surl = 'http://localhost:5000/'; 
+    //var surl = 'http://mighty-brook-9138.herokuapp.com/';
     Video.initialize(surl);
 }
 
@@ -14,11 +16,21 @@ function onytplayerStateChange(newState) {
 
 function getYoutubeID(url) {
     var video_id = url.split('v=')[1];
+    if(!video_id) //differently formatted
+	video_id = url.split('/v/')[1].substring(0,11);
     var ampersandPosition = video_id.indexOf('&');
     if(ampersandPosition != -1) {
 	video_id = video_id.substring(0, ampersandPosition);
     }
     return video_id;
+}
+
+function getTitle(id) {
+    $.ajax({
+	url: 'http://gdata.youtube.com/feeds/api/videos/'+id
+    }).done(function ( data ) {
+	$('#vidtitle').text($(data.getElementsByTagName('title')[0]).text());
+    });
 }
 
 //Create a youtube module to use.
@@ -30,7 +42,7 @@ function getYoutubeID(url) {
 	    this.socket = io.connect(socketURL);
 	    
 	    ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
-	    
+
   	    $('#send').click(function() {
 		Video.loadVideo(getYoutubeID($('#vidurl').val()));
 	    });
@@ -49,14 +61,15 @@ function getYoutubeID(url) {
 	    url = 'http://www.youtube.com/v/' + data.videoID;
 	    console.log(url);
 	    ytplayer.cueVideoByUrl(url);
+	    getTitle(data.videoID);
 	},
 	
 	//Changes video state
 	changeState : function(data) {
 	    console.log('we got the state: ' + data.state);
-	    if(data.state == 1 && ytplayer.getPlayerState != 1)
+	    if(data.state == 1)// && ytplayer.getPlayerState != 1)
 		ytplayer.playVideo();
-	    else if(data.state == 2 && ytplayer.getPlayerState != 2)
+	    else if(data.state == 2)// && ytplayer.getPlayerState != 2)
 		ytplayer.pauseVideo();
 	},
 	
